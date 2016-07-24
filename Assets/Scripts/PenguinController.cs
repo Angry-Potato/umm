fb3a016778ce2df241d6e412747446ff1ec607a0
Angry-Potato <liam.humphreys1989@gmail.com>
2016-07-24 19:28:23 +0100

    asd

--- Assets/Scripts/PenguinController.cs
+++ Assets/Scripts/PenguinController.cs
@@ -4,11 +4,13 @@ using System.Collections;
 public class PenguinController : MonoBehaviour {
 
 	private Rigidbody2D myRigidBody;
+	private Animator myAnim;
 	public float penguinJumpForce = 500f;
 
 	// Use this for initialization
 	void Start () {
 		myRigidBody = GetComponent<Rigidbody2D>();
+		myAnim = GetComponent<Animator>();
 	}
 
 	// Update is called once per frame
@@ -16,5 +18,6 @@ public class PenguinController : MonoBehaviour {
 		if (Input.GetButtonUp("Jump")) {
 			myRigidBody.AddForce(transform.up*penguinJumpForce);
 		}
+		myAnim.SetFloat("vVelocity", myRigidBody.velocity.y);
 	}
 }
